e95b3377fff4edc370e634b947050879b4d64ce6
Ananda Bastakoti <iamanandabastakoti@gmail.com>
2024-06-28 09:27:30 +0545

    added login and register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import { Slide, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
-    <div className='w-full flex justify-center items-center h-screen bg-primaryBg text-primaryText'>
-      MERN Auth
+    <div className='w-full flex justify-center font-Prompt items-center h-screen bg-primaryBg text-primaryText'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        limit={2}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover={false}
+        theme="light"
+        transition={Slide}
+      />
     </div>
   )
 }
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+const Register = () => {
+  const [userData, setUserData] = useState({
+    username: '',
+    email: '',
+    password: ''
+  })
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log(userData)
+    toast.success("Account created successfully")
+  }
+  return (
+    <div className='w-1/3 flex flex-col items-center gap-3'>
+      <h3 className='text-3xl text-center font-Montserrat text-mainColor'>Create an account</h3>
+      <form onSubmit={handleRegister} className='flex flex-col gap-3 w-full'>
+        <div className='flex flex-col gap-1'>
+          <label htmlFor="username">Username</label>
+          <input className='p-2 rounded-lg bg-gray-700 outline-none text-primaryText' type="text" name='username' id='username' value={userData.username} onChange={e => setUserData({ ...userData, username: e.target.value })} placeholder='username' />
+        </div>
+        <div className='flex flex-col gap-1'>
+          <label htmlFor="email">Email</label>
+          <input className='p-2 rounded-lg bg-gray-700 outline-none text-primaryText' type="email" name='email' id='email' value={userData.email} onChange={e => setUserData({ ...userData, email: e.target.value })} placeholder='e.g. yourname@email.com' />
+        </div>
+        <div className='flex flex-col gap-1'>
+          <label htmlFor="password">Password</label>
+          <input className='p-2 rounded-lg bg-gray-700 outline-none text-primaryText' type="password" name='password' id='password' value={userData.password} onChange={e => setUserData({ ...userData, password: e.target.value })} placeholder='password' />
+        </div>
+        <button className='w-full bg-mainColor hover:brightness-90 active:scale-90 duration-500 rounded-lg p-2' type="submit">Register</button>
+      </form>
+      <div>
+        Already have an account? <Link to={'/login'} className='text-mainColor'>Login</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Register
