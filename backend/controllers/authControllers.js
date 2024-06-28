32c8064cf4025fefdda21bca301f6d740d48bb95
Ananda Bastakoti <iamanandabastakoti@gmail.com>
2024-06-28 20:27:46 +0545

    implementing bcrypt to hash password while registering an account

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,4 +1,5 @@
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 const testRoute = (req, res) => {
   res.json({ message: "This is a test route" });
@@ -17,11 +18,14 @@ const getAllUsers = async (req, res) => {
 
 const registerUser = async (req, res) => {
   const { username, email, password } = req.body;
+  //hashing password
+  const salt = await bcrypt.genSalt(10);
+  const hashed_password = await bcrypt.hash(password, salt);
   try {
     const newUser = await User.create({
       username,
       email,
-      password,
+      password: hashed_password,
     });
     console.log("User registered successfully");
     res.status(200).json(newUser);
